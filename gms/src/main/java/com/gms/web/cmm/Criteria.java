cfa09792a08fcbe7c2c6c439bea0b8b5f7ff157b
rlawjdgnsdle <meetshrimpsheese@gmail.com>
2018-09-21 11:31:47 +0900

    2018.09.21. AM

--- gms/src/main/java/com/gms/web/cmm/Criteria.java
+++ gms/src/main/java/com/gms/web/cmm/Criteria.java
@@ -3,8 +3,7 @@ package com.gms.web.cmm;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import com.gms.web.reply.Reply;
-
+//
 import lombok.Data;
 
 @Component
